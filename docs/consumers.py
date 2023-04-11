5e2c377798e9ea4f999945adf93f0c7f0dcde2f9
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-11 23:18:28 +0300

    docs/consumers: Replace redundent function for just code

--- docs/consumers.py
+++ docs/consumers.py
@@ -1,4 +1,6 @@
 import json
+import subprocess
+
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
@@ -15,17 +17,29 @@ class ChatConsumer(WebsocketConsumer):
         
         
     def receive(self, text_data):
+
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        
-        async_to_sync(self.channel_layer.group_send)(
-        
-            self.room_group_name,
-            {
-                'type':'chat_message',
-                'message': message
-            }
-    ) 
+            
+        if 'message' in text_data_json:
+            message = text_data_json['message']
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type':'chat_message',
+                    'message': message
+                }
+            )
+        elif 'content' in text_data_json:
+            input_string = text_data_json['content']
+            process = subprocess.Popen(['python', '-c', input_string], stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            output = output.decode()
+            self.send(text_data=json.dumps({
+                'type': 'input_response',
+                'message': output
+            }))
+
+            
     
     def chat_message(self, event):
         message = event['message']
@@ -34,3 +48,4 @@ class ChatConsumer(WebsocketConsumer):
             'type': 'chat',
             'message': message            
         }))
+
