981e9c605dc3013347fc77c9a9fa9b932e0ee8c4
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-21 11:43:59 +0300

    docs/consumers: Fixed formatting

--- docs/consumers.py
+++ docs/consumers.py
@@ -5,6 +5,7 @@ import threading
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = 'test'
@@ -24,48 +25,40 @@ class ChatConsumer(WebsocketConsumer):
 
         if 'message' in text_data_json:
             message = text_data_json['message']
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'chat_message',
-                    'message': message
-                }
-            )
+            self.send_chat_message(message)
         elif 'content' in text_data_json:
             content = text_data_json['content']
             if self.process and self.process.poll() is None:
-                self.send(text_data=json.dumps({
-                    'type': 'input_response',
-                    'message': 'Code is already running. Please wait for it to finish before submitting new code.'
-                }))
+                self.send_input_response('Code is already running. Please wait for it to finish before submitting new code.')
             else:
-                self.process = subprocess.Popen(['python', '-c', content], stdout=subprocess.PIPE)
-                self.thread = threading.Thread(target=self._output_reader)
-                self.thread.start()
+                self.run_code(content)
         elif 'input_response' in text_data_json:
             input_response = text_data_json['input_response']
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'input_response_message',
-                    'message': input_response
-                }
-            )
+            self.send_input_response_message(input_response)
 
-    
+    def run_code(self, content):
+        self.process = subprocess.Popen(['python', '-c', content], stdout=subprocess.PIPE)
+        self.thread = threading.Thread(target=self._output_reader)
+        self.thread.start()
+        
+
     def _output_reader(self):
-        while True:
-            line = self.process.stdout.readline().decode()
-            if not line:
-                break
-            self.output += line
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'input_response_message',
-                    'message': self.output
-                }
-            )
+        for line in iter(self.process.stdout.readline, b''):
+            self.output += line.decode()
+            self.send_input_response_message(self.output)
+
+        self.process.stdout.close()
+        self.process.wait()
+        self.process = None
+
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
 
     def chat_message(self, event):
         message = event['message']
@@ -74,12 +67,24 @@ class ChatConsumer(WebsocketConsumer):
             'message': message
         }))
 
-    def input_response_message(self, event):
-        message = event['message']
+    def send_input_response(self, message):
         self.send(text_data=json.dumps({
             'type': 'input_response',
             'message': message
         }))
+
+    def send_input_response_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'input_response_message',
+                'message': message
+            }
+        )
+
+    def input_response_message(self, event):
+        message = event['message']
+        self.send_input_response(message)
 
     def disconnect(self, close_code):
         if self.process and self.process.poll() is None:
@@ -91,5 +96,3 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-
-
