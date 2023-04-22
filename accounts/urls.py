0e319e13a7a19e1cbeb85bd5cf42fbf96a43da97
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-22 14:38:42 +0300

    accounts/urls: Add support for the register and login pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import register, login
+from .views import register, login_user, home
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', login_user, name='login_user'),
+    path('', home, name='home')
 ]
