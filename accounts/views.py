4de940969046c5c620fabb7ceb555ee4e6486854
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-26 14:22:00 +0300

    accounts/views: Rewrite `signup` views

--- accounts/views.py
+++ accounts/views.py
@@ -1,49 +1,29 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from .models import User
-from django.contrib.auth.models import User
-from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.db.models import Q
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 # Create your views here.
-def register(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        school = request.POST['school']
-        password = request.POST['password']
-        confirm_pass = request.POST['confirm-pass']
+def signup(request):
+    form = SignUpForm()
 
-        account = User.objects.create_user(username=username, email=email, password=password)
+    if request.method == 'POST':   
+        form = SignUpForm(request.POST)
         
-        account.save()
-        
-        messages.success(request, 'You Successfully Registered An Account :D')
+        if form.is_valid():
+            form.save()
+            
+            user = form.save()
              
-        return redirect('login_user')
-    
-    return render(request, 'accounts/register.html')
+            login(request, user)
+            
+            return redirect('home')
+        
+    return render(request ,'accounts/signup.html', context= {'form': form})
 
 
 def login_user(request):
-    if request.method == 'POST':
-        username_or_email = request.POST['username']
-        password = request.POST['password']
-        
-        user = User.objects.filter(Q(username=username_or_email) | Q(email=username_or_email)).first()
-        
-        if user and user.check_password(password):
-            login(request, user)
-            
-            return render(request, 'home.html', {'username': user.username})
-
-        else:
-            messages.error(request, "Try Again!")
-            
-            return redirect('login_user')
-           
-    return render(request, 'accounts/login.html')
+    pass
     
         
 def home(request):
