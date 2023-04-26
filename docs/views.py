f5dd00b17e84504e01d8e5a0155701546cfe4133
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-26 14:23:27 +0300

    docs/views: Add `create_project` view

--- docs/views.py
+++ docs/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def ide(request):
     return render(request, 'docs/ide.html') 
+
+
+def create_project(request):
+    return render(request, 'docs/create_project.html')
