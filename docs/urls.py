18bf868fc540e1c68488950d4ee6c1fcc9608a73
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-27 01:24:16 +0300

    docs/urls: Add both links to one source

--- docs/urls.py
+++ docs/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.urls import re_path
+from . import consumers
 
 urlpatterns = [
     path('ide/', views.ide),
@@ -7,3 +9,9 @@ urlpatterns = [
     path('create/', views.create_project),
     path('views_projects', views.view_projects)
 ]
+
+websocket_urlpatterns = [
+    re_path(r'ws/<str:project_name>/', consumers.ChatConsumer.as_asgi())
+]
+
+urlpatterns += websocket_urlpatterns
