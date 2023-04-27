47b39132a1c5514277d709f0b024d654559f6d6b
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-27 20:56:03 +0300

    docs/views: Add good logic

--- docs/views.py
+++ docs/views.py
@@ -1,23 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.utils.text import slugify
 from .models import Room
 
 # Create your views here.
+@login_required(login_url='/login/')
 def ide(request):
     return render(request, 'docs/ide.html') 
 
-@login_required
-def create_project(request):
-    return render(request, 'docs/create_project.html')
 
-@login_required
+@login_required(login_url='/login/')
+def create_project(request):
+    if request.method == 'POST':
+        name = request.POST['project_name']
+        description = request.POST.get('description', '')
+        
+        slug = slugify(name)
+        
+        # Create new Room object with generated slug
+        project = Room.objects.create(name=name, slug=slug, description=description)
+        return redirect('/docs/view_projects/')
+    else:
+        return render(request, 'docs/create_project.html')
+
+
+
+@login_required(login_url='/login/')
 def view_projects(request):
     projects = Room.objects.all()
 
     return render(request, 'docs/view_projects.html', {'projects': projects})
 
-@login_required
+@login_required(login_url='/login/')
 def project_ide(request, slug):
-    project = Room.objects.get(slug=slug)
-
+    project = get_object_or_404(Room, slug=slug)
     return render(request, 'docs/ide.html', {'project': project})
