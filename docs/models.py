f5196a5834e73df575f13e108fffb9cd35db6ba0
LiranBratt2121 <brattlirannin@gmail.com>
2023-04-27 20:54:32 +0300

    docs/models: Refraze Room model

--- docs/models.py
+++ docs/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, default='Cool project')
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
