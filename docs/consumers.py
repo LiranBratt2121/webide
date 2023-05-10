2f417e95c1ebaeec181af1a951eaacd3a90103e9
LiranBratt2121 <brattlirannin@gmail.com>
2023-05-10 23:15:08 +0300

    docs: consumers: Add `STOP` button support & Fix socket

--- docs/consumers.py
+++ docs/consumers.py
@@ -1,7 +1,7 @@
+import asyncio
 import json
 import subprocess
 import threading
-from random import randint
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
@@ -26,53 +26,47 @@ class ChatConsumer(AsyncWebsocketConsumer):
             message = text_data_json['message']
             await self.send_chat_message(message)
         elif 'content' in text_data_json:
+            print('In content')
             content = text_data_json['content']
             if len(content) > 100000:
-                await self.send_input_response_message('Error: Message is too long.')
+                await self.group_send_input_response_message('Error: Message is too long.')
                 return
             if self.process and self.process.poll() is None:
-                await self.send_input_response_message('Code is already running. Please wait for it to finish before submitting new code.')
+                await self.group_send_input_response_message('Code is already running. Please wait for it to finish before submitting new code.')
             else:
                 try:
                     compile(content, '<string>', 'exec')
                 except SyntaxError:
-                    await self.send_input_response_message('Error: Invalid Python code.')
+                    await self.group_send_input_response_message('Error: Invalid Python code.')
                     return
                 self.output = ''  # clear the output before sending new input
                 await self.run_code(content)
         elif 'input_response' in text_data_json:
             input_response = text_data_json['input_response']
-            await self.send_input_response_message(input_response)
+            await self.group_send_input_response_message(input_response)
         elif 'clear' in text_data_json:
             self.output = ''
-            await self.send_input_response_message('')
+            await self.group_send_input_response_message('')
         elif 'stop' in text_data_json:
             if self.thread and self.thread.is_alive():
                 self.process.terminate()
-                self.process = None
-                await self.send_input_response_message('Process stopped by user.')
-                self.thread.join()
-                self.thread = None
-                self.output = ''
+            await self.group_send_input_response_message('Process stopped by user.')
 
-    async def run_code(self, content):
-        self.send_input_response_message('bro code;')
-        self.thread = threading.Thread(target=self._run_code_thread, args=(content,))
-        self.thread.start()
 
-    def _run_code_thread(self, content):
+    async def _run_code_thread(self, content):
+        print('In Run code Thread!')
         self.process = subprocess.Popen(['python', '-c', content], stdout=subprocess.PIPE)
         for line in iter(self.process.stdout.readline, b''):
             self.output += line.decode()
             if len(self.output) > 100000:
-                self.send_input_response_message('Error: Output is too long.')
+                self.group_send_input_response_message('Error: Output is too long.')
                 self.process.terminate()
                 self.process = None
                 self.thread = None
-                self.send_input_response_message('Process killed due to excessive output.')
+                self.group_send_input_response_message('Process killed due to excessive output.')
                 self.output = ''
                 return
-            self.send_input_response_message(self.output)
+            await self.group_send_input_response_message(self.output)
 
         self.thread = None
 
@@ -81,23 +75,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.process.wait()
             self.process = None
 
+    async def run_code(self, content):
+        print('Tried Running code')
+        coro = self._run_code_thread(content)
+        print('Started Thread')
+        await asyncio.create_task(coro)
 
-    # async def chat_message(self, message): # Used
-    #     await self.send(text_data=json.dumps({
-    #         'type': 'chat_message',
-    #         'message': message
-    #     }))
+
+    # Update terminal functions
+    async def input_response_message(self, event):
+        input_response = event
+
+        await self.send(text_data=json.dumps({
+            'type': 'update_terminal',
+            'message': input_response
+        }))
         
 
-    async def send_input_response_message(self, input_response):
+    async def group_send_input_response_message(self, message):
+        event = message
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'input_response_message',
-                'message': input_response
+                'message': event
             }
         )
+
         
+    # Update text-area functions
+    async def chat_message(self, event):
+        message = event['message']
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message': message
+        }))
+
     async def send_chat_message(self, message):
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -106,12 +120,3 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'message': message
             }
         )
-
-    async def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'type': 'chat_message',
-            'message': message
-        }))
