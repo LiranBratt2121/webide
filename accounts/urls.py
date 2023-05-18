9254890e9e6cd432bffc5dbf1dae0010d114a0b1
LiranBratt2121 <brattlirannin@gmail.com>
2023-05-18 21:39:19 +0300

    change drop point to website

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('home/', views.home, name='home')
+    path('', views.home, name='home')
 ]
