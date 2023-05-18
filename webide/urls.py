de0e26a67d8d7942e7376b21e6afb3b74518a5e7
LiranBratt2121 <brattlirannin@gmail.com>
2023-05-18 23:27:05 +0300

    reverte home chage

--- webide/urls.py
+++ webide/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', include('docs.urls')),  
-    path('', include('accounts.urls')),  
+    path('/home/', include('accounts.urls')),  
 ]
