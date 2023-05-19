13ce12d01ed4610620738151e91d254e51275aa9
LiranBratt2121 <brattlirannin@gmail.com>
2023-05-19 21:56:39 +0300

    wip

--- docs/models.py
+++ docs/models.py
@@ -4,6 +4,13 @@ class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, default='Cool project')
     description = models.TextField(blank=True)
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=True, default='''
+def main():
+    # Your code goes here
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
+''')
     def __str__(self):
         return self.name
